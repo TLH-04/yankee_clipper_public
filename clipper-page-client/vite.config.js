917f4f106d1471f2cfabc9e8a7062a00f3fd34ba
TLH-04 <TrinityLHampton@gmail.com>
2026-01-13 09:20:33 -0500

    Disabiling Jekyll

--- clipper-page-client/vite.config.js
+++ clipper-page-client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/yankee_clipper_public/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
