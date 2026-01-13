0ba7320abec2c0b67c345ec9e63f2178969b810a
TLH-04 <TrinityLHampton@gmail.com>
2026-01-13 09:30:42 -0500

    Fixing pages

--- clipper-page-client/vite.config.js
+++ clipper-page-client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/yankee_clipper_public/',
   build: {
-    outDir: 'docs',
+    outDir: path.resolve(__dirname, '../docs'),
     emptyOutDir: true
   }
 })
